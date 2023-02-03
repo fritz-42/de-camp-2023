1d3a5d75d1475507416b00b1390b20d4275c159d
fritz-42 <fritz@schauermann.org>
2023-02-03 10:28:57 +0100

    adapt data preprocessing for GREEN taxi dataset

--- etl_web_to_gcs_params.py
+++ etl_web_to_gcs_params.py
@@ -16,18 +16,19 @@ def fetch( dataset_url: str ) -> pd.DataFrame:
 @task( log_prints = True )
 def clean( df = pd.DataFrame ) -> pd.DataFrame:
     """Fix data format issues"""
-    df['tpep_pickup_datetime']  = pd.to_datetime( df['tpep_pickup_datetime'] )
-    df['tpep_dropoff_datetime'] = pd.to_datetime( df['tpep_dropoff_datetime'] )
+    # df['tpep_pickup_datetime']  = pd.to_datetime( df['tpep_pickup_datetime'] )
+    # df['tpep_dropoff_datetime'] = pd.to_datetime( df['tpep_dropoff_datetime'] )
 
     print( df.head(2) )
     print( f"columns: {df.dtypes}" )
     print( f"rows: {len(df)}" )
     return df
 
-@task()
+@task( log_prints = True )
 def write_local( df: pd.DataFrame, color: str, dataset_file: str ) -> Path:
     """Write DataFrame out locally as parquet file"""
     path = Path( f"data/{color}/{dataset_file}.parquet" )
+    print( f"write data to path: {path}" )
     df.to_parquet( path, compression = 'gzip' )
     return path
 
